0ba4723f731338654698bab2b0bf3b78b04e1ab1
Ивлев Вадим <ivlev@rg.ru>
2020-01-29 16:05:27 +0300

    fix: simplified .gitlab-ci.yml 3

--- .import-ssh-key.sh
+++ .import-ssh-key.sh
@@ -10,8 +10,8 @@ echo '-----------------importing ssh-key--------------------------'
 which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
 eval $(ssh-agent -s)
 # Attention! Key need convert in base64 and add in secret variable DEPLOY_RSA_PRIVATE_KEY
-ssh-add <(echo $1 | base64 --decode)
+ssh-add <(echo "$1" | base64 --decode)
 mkdir -p /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-echo $1 | base64 --decode > /root/.ssh/id_rsa
+echo "$1" | base64 --decode > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa && chmod 700 /root/.ssh
--- README.md
+++ README.md
@@ -4,5 +4,3 @@
 
 http://prometheus:9090
 
-
-<iframe src="https://monitor.rg.ru/d-solo/toopdhyWk/servisy-rg?orgId=1&refresh=5s&from=1580299286994&to=1580302886994&var-job=auth-proxy&var-job=file-uploader&theme=light&panelId=20" width="450" height="200" frameborder="0"></iframe>
